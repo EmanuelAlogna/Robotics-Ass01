da915205f91c900e901cab3d923e0d18de9c8319
EmanuelAlogna <emanuel.alogna@gmail.com>
2019-05-27 01:07:09 +0200

    Odometry computation added

--- src/my_car/src/car_listener.cpp
+++ src/my_car/src/car_listener.cpp
@@ -24,6 +24,7 @@
 #define foreach BOOST_FOREACH
 
 using namespace message_filters;
+#define PI 3.14159265
 
 int count = 1;
 
@@ -50,18 +51,79 @@ double rear_wheels_baseline;
 
 float x_init;
 float y_init;
-float theta;
+float theta = 0.0;
+float alpha;
 int odometry_model;
 int i = 0;
 
 float vx = 100;  // just random values for now.
 float vy = 100;
 float vth = 100;
+ros::Time current_time;
+ros::Time last_time;
+
+double Ts = 0;
+double last_time_R = 0;
+
+
+// I want two variables to store the old values of x_config and y_config so
+// that if values change through dynamic reconfigure I can notice that.
+double dynamic_x_old;
+double dynamic_y_old;
+// let's store also old value of theta
+double dynamic_theta_old;
+
+
 
 void compute_ackerman(){
+
+	// alpha = steer/steering_factor
+	double v = (speed_R + speed_L)/2;
+	double R = (distance_front_rear / tan((alpha*PI)/180));
+	
+	vth = v/R;
+	//compute x, y, th, using Ts (also called dt)
+	//double theta_rad = th*PI)/180  //from degree to radiant -> th*(2*pi)/360
+	
+	x_init = x_init + v * Ts * cos(th);
+	y_init = y_init + v * Ts * sin(th);
+	vth = (vth*180)/PI;
+	th = th + vth*Ts;
+
+	
+	ROS_INFO("v: [%f]", v);
+	ROS_INFO("vth: [%f]", vth);
+	ROS_INFO("alpha: [%f]", alpha);
+	ROS_INFO("x': [%f]", x_init);
+	ROS_INFO("y': [%f]", y_init);
+	ROS_INFO("theta': [%f]\n", th);
 }
 
 void compute_differential_drive_kinematics(){
+	
+	double v = (speed_R + speed_L)/2;
+	
+	if (speed_R > speed_L)   // car is rotating w/ positive omega (regola mano dx)
+		vth = (speed_R-speed_L)/rear_wheels_baseline;
+	else if (speed_R < speed_L)
+		vth = (speed_L-speed_R)/rear_wheels_baseline;
+	else
+		vth = 0; // vehicle is not rotating
+
+	//compute x, y, th, using Ts (also called dt)
+	//double theta_rad = th*PI)/180  //from degree to radiant -> th*(2*pi)/360
+	
+	x_init = x_init + v * Ts * cos(th);
+	y_init = y_init + v * Ts * sin(th);
+	th = th + ((vth*180)/PI)*Ts;
+
+	ROS_INFO("v: [%f]", v);
+	ROS_INFO("vth: [%f]", vth);
+	ROS_INFO("x': [%f]", x_init);
+	ROS_INFO("y': [%f]", y_init);
+	ROS_INFO("theta': [%f]", th);
+	
+	
 }
 
 
@@ -76,8 +138,11 @@ void dynamic_reconfigure_callback(my_car::parametersConfig &config, uint32_t lev
 		else 
 			ROS_INFO("Odometry computed through Diff. Drive Kinematics");
 		x_init = config.x_init;
+		dynamic_x_old = config.x_init;
 		y_init = config.y_init;
+		dynamic_y_old = config.y_init;
 		theta = config.theta;
+		dynamic_theta_old = config.theta;
 		odometry_model = config.odometry;
 		
 		// the default set in the parameters.cfg is (0,0) for (x,y). 0 for theta
@@ -88,18 +153,23 @@ void dynamic_reconfigure_callback(my_car::parametersConfig &config, uint32_t lev
 	
 	// LET'S CHECK IF ODOMETRY MODE IS CHANGED OR IF THE STARTING POINT IS RESET
 	
-	if (x_init != config.x_init){
-		x_init = config.x_init;  // i change globabl var value that used for the odom.
+	
+	// check if value changed
+	if (dynamic_x_old != config.x_init){
+		x_init = config.x_init;
+		dynamic_x_old = config.x_init;
 		ROS_INFO("Starting point - x coord changed to: [%0.1f]", config.x_init);
 	}
 		
-	if (y_init != config.y_init){
-		y_init = config.y_init;
+	if (dynamic_y_old != config.y_init){
+		y_init = config.y_init;         // I change the value of the y coordinate
+		dynamic_y_old = config.y_init;  // I store the value that I'll use later
 		ROS_INFO("Starting point - y coord changed to: [%0.1f]", config.y_init);
 	}
 	
-	if (theta != config.theta){
+	if (dynamic_theta_old != config.theta){
 		theta = config.theta;
+		dynamic_theta_old = config.theta;
 		ROS_INFO("Starting point - th coord changed to: [%0.1f]", config.theta);
 	}
 	
@@ -121,20 +191,47 @@ bool inside = false;
 void callback(ros::NodeHandle &n, const my_car::floatStamped::ConstPtr& speed_wheel_L, const my_car::floatStamped::ConstPtr& speed_wheel_R, const my_car::floatStamped::ConstPtr& steer)
 {
 	//ROS_INFO("I heard: [%0.2f]", steer->data);
+	if (!inside){
+		last_time_R = speed_wheel_R->header.stamp.toSec();  // initializing last_time_R
+		// first time, there will be Ts = 0
+	}
    	inside = true;
-	th = (steer->data)/steering_factor;
+	alpha = (steer->data)/steering_factor;
     speed_L = speed_wheel_L->data;
     speed_R = speed_wheel_R->data;
     
     vx = speed_L;
     vy = speed_R;
-    vth = th;  // ?? not sure about this
+    vth = alpha;  // ?? not sure about this
     
     
-    //ROS_INFO("Angle theta: [%0.2f]", th);
-    //ROS_INFO("Speed left wheel: [%0.2f]", speed_L);
-    //ROS_INFO("Speed right wheel: [%0.2f]\n", speed_R);
+    double timestamp_R = speed_wheel_R->header.stamp.toSec();
+    
+    
+    //ROS_INFO("Previous timestamp : [%f]", last_time_R);
+    //ROS_INFO("Timestamp of speed_wheel_R : [%f]\n", timestamp_R);
+    
+    // let's compute dt
+    
+    Ts = timestamp_R - last_time_R;
+    
+    //ROS_INFO("Ts : [%f]\n", Ts);
+    
+    /*
+    ROS_INFO("Angle of steering: [%0.2f]", alpha);
+    ROS_INFO("Speed left wheel: [%0.2f]", speed_L);
+    ROS_INFO("Speed right wheel: [%0.2f]\n", speed_R);
   
+  	float b = rear_wheels_baseline/2;
+  	float R = (distance_front_rear / tan((th*PI)/180));
+  	
+  	float w1 = speed_L/(R+b);
+  	float w2 = speed_R/(R-b);
+  	
+  	ROS_INFO("W computed from left speed: [%f]", w1);
+  	ROS_INFO("W computed from right speed: [%f]", w2);
+  	*/
+  	
   	// we have 1285 tuples of speedL, speedR, theta. 13013
   
  	// DYNAMIC RECONFIGURE
@@ -149,10 +246,12 @@ void callback(ros::NodeHandle &n, const my_car::floatStamped::ConstPtr& speed_wh
     // I need all the parameters set previously
    
     if (odometry_model == 1){
-		compute_ackerman();
+		compute_differential_drive_kinematics();
+		ROS_INFO("Ackerman:");
 	}
     else{
    		compute_differential_drive_kinematics();
+		ROS_INFO("Differential drive");
     }
    
     // PRINT ONE MORE TIME DATA WE NEED: X,Y,THETA, VX, VY, VTHETA
@@ -169,6 +268,8 @@ void callback(ros::NodeHandle &n, const my_car::floatStamped::ConstPtr& speed_wh
 	
 	// NOW I CAN PUBLISH EVERYTHING IN THE ODOM TOPIC
 	// see main code block
+	
+	last_time_R = timestamp_R;
    
 }
 
@@ -190,7 +291,7 @@ int main(int argc, char **argv){
 	
 	//ros::Rate r(1.0);
 	
-	ros::Time current_time, last_time;
+	
   	current_time = ros::Time::now();
   	last_time = ros::Time::now();
   	
@@ -227,9 +328,9 @@ int main(int argc, char **argv){
  	while (ros::ok()){
  		if (inside){  // so if the bag has started playing
 			
-			ROS_INFO("Speed left wheel: [%0.2f]", vx);
-			ROS_INFO("Speed left wheel: [%0.2f]", vy);
-			ROS_INFO("Speed left wheel: [%0.2f]\n", vth);
+			//ROS_INFO("Speed left wheel: [%0.2f]", vx);
+			//ROS_INFO("Speed left wheel: [%0.2f]", vy);
+			//ROS_INFO("Speed left wheel: [%0.2f]\n", vth);
 			
 			// SO APPARENTLY THETA IS NOT USED
 			
@@ -265,7 +366,7 @@ int main(int argc, char **argv){
 			odom.child_frame_id = "my_car";
 			odom.twist.twist.linear.x = vx;
 			odom.twist.twist.linear.y = vy;
-			odom.twist.twist.angular.z = vth;
+			odom.twist.twist.angular.z = vth; // vth is alpha
 
 			//publish the message
 			odom_pub.publish(odom);
